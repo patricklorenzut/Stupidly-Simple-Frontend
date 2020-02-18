4efe3e9ce3dee8bf858d7c2c4655bfe809ee2e40
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2020-02-18 15:07:58 -0500

    twist and shout

---
+++ pages/twister.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div>        
+
+    <div>
+      <header class="max-w-4xl mx-auto text-center py-20 lg:py-40 px-10">
+        <h1 class="text-5xl font-bold leading-tight">
+          Shamelessly Steal Attention From Your Website Visitors
+        </h1>
+        <h2 class="text-2xl mt-4">
+          Change your page title when visitors leave your website. Because the internet is really fucking boring, and maybe this will make somebody laugh. Which will then lead to ridiculous profits, obviously.
+        </h2> 
+
+        <div class="mt-16">         
+            <p class="mb-3">Here’s the code:</p>
+            <div class="bg-gray-800 p-4 max-w-md mx-auto font-mono text-white text-sm overflow-x-scroll text-left">
+            <pre><code>&lt;script type=&quot;text/javascript&quot;&gt;
+    window.UnderpolishedTitles = &quot;Your mom. 👩&zwj;🦰///Come baaaack!!!///Hey. 😗///I miss you. 😍///🤢 That site sucks!&quot;
+&lt;/script&gt;
+&lt;script src=&quot;https://api.underpolished.com/js/twister.js&quot; async type=&quot;text/javascript&quot;&gt;&lt;/script&gt;</code></pre>
+            </div>
+        </div>
+
+      </header>  
+    </div>  
+
+    <section class="py-20 pt-10 px-10 max-w-xl mx-auto">
+
+        <h2 class="text-2xl font-bold mt-16 mb-2">The Deets</h2>
+                            
+        <ul>
+            <li class="my-4">Copy and paste the code above into the footer of your website, changing the <code class="text-red-400 bg-gray-800">window.UnderpolishedTitles</code> to whatever you want.</li>
+            <li class="my-4">Separate each prompt with <code class="text-red-400 bg-gray-800">///</code>.</li>
+            <li class="my-4">Each time the user leaves your page, the title will randomly cycle through the text prompts. Try it out by leaving this page and coming back multiple times.</li>
+            <li class="my-4"><a href="mailto:patrick.lorenzut@gmail.com">Email me</a> with a link to your site after you install this, and if I laugh then I'll add your website here.</li>
+        </ul>
+
+        <p class="my-4">That’s all there is to it. Questions? Feedback? <span class="text-red-500 hover:text-red-900 cursor-pointer" onclick="underpolished_chat.open()">Let me know!</span></p>
+
+        <p class="my-4">Inspired by <a href="https://growthexamples.com/" class="text-red-500 hover:text-red-900 cursor-pointer" target="_blank">https://growthexamples.com/</a></p>
+
+    </section>    
+
+  </div>
+</template>
